182e21782563f17e1c5d730279aa513985c2c5bb
Taylor Fausak <taylor@fausak.me>
2022-01-27 07:51:20 -0500

    Format everything with Brittany

--- source/library/HW.hs
+++ source/library/HW.hs
@@ -9,12 +9,23 @@ import qualified Network.Wai.Handler.Warp as Warp
 import qualified Paths_hw as Package
 
 main :: IO ()
-main = Warp.runSettings Warp.defaultSettings $ \ request respond ->
+main = Warp.runSettings Warp.defaultSettings $ \request respond ->
     case Text.unpack <$> Wai.pathInfo request of
         ["favicon.ico"] ->
             case Http.parseMethod $ Wai.requestMethod request of
                 Right Http.GET -> do
                     filePath <- Package.getDataFileName "favicon.ico"
-                    respond $ Wai.responseFile Http.ok200 [(Http.hContentType, Encoding.encodeUtf8 $ Text.pack "image/x-icon")] filePath Nothing
-                _ -> respond $ Wai.responseLBS Http.methodNotAllowed405 [] LazyByteString.empty
-        _ -> respond $ Wai.responseLBS Http.notFound404 [] LazyByteString.empty
+                    respond $ Wai.responseFile
+                        Http.ok200
+                        [ ( Http.hContentType
+                          , Encoding.encodeUtf8 $ Text.pack "image/x-icon"
+                          )
+                        ]
+                        filePath
+                        Nothing
+                _ -> respond $ Wai.responseLBS
+                    Http.methodNotAllowed405
+                    []
+                    LazyByteString.empty
+        _ ->
+            respond $ Wai.responseLBS Http.notFound404 [] LazyByteString.empty
